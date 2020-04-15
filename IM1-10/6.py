0afb7b8dfdb1e3465cf2ce82de858c39d9a3e13d
hirokichi-k <hiroki@DESKTOP-6R80CLM.localdomain>
2020-04-15 11:08:28 +0900

    04_17_1st

--- IM1-10/6.py
+++ IM1-10/6.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 def reduce(img):
-    img[(img >= 0) & (img < 64)] = 32
-    img[(img >= 64) & (img < 128)] = 96
-    img[(img >= 128) & (img < 192)] = 160
-    img[(img >= 192) & (img < 256)] = 224
+    # img[(img >= 0) & (img < 64)] = 32
+    # img[(img >= 64) & (img < 128)] = 96
+    # img[(img >= 128) & (img < 192)] = 160
+    # img[(img >= 192) & (img < 256)] = 224
+    img = img // 64 * 64 + 32
     return img
 
 img = cv2.imread('imori.jpg').astype(np.float32)
