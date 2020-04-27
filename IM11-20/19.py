4f304831c12b5e80dedba17f836006fca4753ab0
hirokichi-k <hiroki@DESKTOP-6R80CLM.localdomain>
2020-04-27 22:14:14 +0900

    0427_2nd

--- IM11-20/19.py
+++ IM11-20/19.py
@@ -2,21 +2,25 @@ import cv2
 import numpy as np
 
 def gray(img):
-    b=img[:,:,0]
-    g=img[:,:,1]
-    r=img[:,:,2]
-    out = np.sum(0.2126 * r, 0.7152 * g, 0.0722 * b)
-    out = out.astype(np.nint8)
+    img[:,:,0] *= 0.0722
+    img[:,:,1] *= 0.7152
+    img[:,:,2] *= 0.2126
+    out = np.sum(img)
+    out = out.astype(np.uint8)
     return out
 
-def LoG(img):
+def LoG(img, K_size = 5, sigma = 3):
+    H,W = img.shape
+    
+    pad = K_size//2
+    
     out = img
     return out
 
 img = cv2.imread("imori_noise.jpg")
 
 img1 = gray(img)
-img2 = LoG(img1)
+img2 = LoG(img1, K_size = 5, sigma = 3)
 
 cv2.imwrite("out_figure19.jpg",img2)
 
